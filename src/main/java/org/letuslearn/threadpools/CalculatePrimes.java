9b3461f61bfd6c8fe3fe1ec8fd96f6acb737b3df
prasadram <prasad.ram126@gmail.com>
2018-12-30 11:24:48 +0530

    Basic thread pool example
     On branch master
     Changes to be committed:
    	modified:   .gitignore
    	new file:   src/main/java/org/letuslearn/threadpools/CalculatePrimes.java
    	new file:   src/main/java/org/letuslearn/threadpools/PrimeCalculator.java

---
+++ src/main/java/org/letuslearn/threadpools/CalculatePrimes.java
@@ -0,0 +1,49 @@
+package org.letuslearn.threadpools;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
+
+public class CalculatePrimes {
+
+/*
+    How Executors internally creates Threads and manages thread life cycle -- check more
+    like how it is re using threads even after completing the task ideally thread should be dead
+
+    Check more about `ThreadPoolExecutor`  at below documentation it has more information about Queuing, Rejection,Hook methods
+    https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ThreadPoolExecutor.html
+*/
+
+    public static void main(String[] args) {
+        /* if you want to change Thread pool name , group and priorities then you should pass `ThreadFactory`
+         implementation to Executors
+         ThreadFactory is responsible for creating threads on demand*/
+        ThreadFactory namedThreadFactory =
+                new ThreadFactoryBuilder().setNameFormat("prime-calculation-thread-%d").build();
+        ExecutorService eService = Executors.newFixedThreadPool(5, namedThreadFactory);
+        Future<Integer>[] results = new Future[5];
+
+        int start = 1;
+        int end = 10000;
+
+        for(int i=0; i < 5; i++) {
+            /*Callable will return the result to main thread*/
+            PrimeCalculator primeCalculator = new PrimeCalculator(start, end);
+            start = end;
+            end += 10000;
+            results[i] = eService.submit(primeCalculator);
+        }
+
+        for(int i=0; i < 5; i++) {
+            try {
+                Integer integer = results[i].get(); // blocks until return values are available
+                System.out.println(integer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        eService.shutdown();
+    }
+}
---
+++ src/main/java/org/letuslearn/threadpools/PrimeCalculator.java
@@ -0,0 +1,41 @@
+package org.letuslearn.threadpools;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.Callable;
+
+public class PrimeCalculator implements Callable<Integer> {
+  /*Callable will return the result to main thread*/
+  private int start;
+  private int end;
+
+  public PrimeCalculator(int start, int end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  @Override
+  public Integer call() throws Exception {
+    int count = 0;
+    System.out.println(
+            Thread.currentThread().getName() + " " + new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date()));
+
+    for (; start < end; start++) {
+      if (isPrime(start)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  private boolean isPrime(int number) {
+    int sqrt = (int) Math.sqrt(number);
+    for (int i = 2; i <= sqrt; i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
+
+  }
+}
